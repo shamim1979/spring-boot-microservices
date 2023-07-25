fcce5ad5e7d23032bd148e4697f1b3afb695ae52
Md Shah Alam Shamim <shamim.cse.ewu@gmail.com>
2023-07-25 18:14:30 +0600

    authorization server is added

---
+++ authorization-server/src/main/java/bd/gov/lims/authorization/AuthorizationServerApplication.java
@@ -0,0 +1,11 @@
+package bd.gov.lims.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+}
